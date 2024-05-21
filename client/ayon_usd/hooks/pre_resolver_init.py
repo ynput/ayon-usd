21c645ae9b2e3dd409c890c41a51f7752bd34937
Ondřej Samohel <ondrej.samohel@ynput.io>
2024-05-21 14:16:24 +0200

    :art: working version of hook

--- client/ayon_usd/hooks/pre_resolver_init.py
+++ client/ayon_usd/hooks/pre_resolver_init.py
@@ -55,35 +55,36 @@ class InitializeAssetResolver(PreLaunchHook):
 
     def execute(self):
         """Pre-launch hook entry method."""
-        resolver_settings = self.data["project_settings"]["usd"]["asset_resolvers"]  # noqa: E501
-        # project_name = self.data["project_name"]
-        host_name = self.application.host_name
-        variant = self.application.variant
+        resolver_settings = self.data["project_settings"]["ayon_usd"]["asset_resolvers"]  # noqa: E501
+        self.log.debug(self.app_group)
 
-        host_variant = f"{host_name}/{variant}"
         for resolver in resolver_settings:
-            if resolver["app_name"] != host_variant:
+            if resolver["app_name"] != self.app_name:
                 continue
 
             if resolver["platform"].lower() != system().lower():
                 continue
 
         if all(
-            host_variant not in resolver["app_name"]
+            self.app_name not in resolver["app_name"]
             for resolver in resolver_settings
         ):
-            self.log.info(f"No asset resolver settings for {host_variant}.")
+            self.log.info(
+                f"No USD asset resolver settings for {self.app_name}.")
             return
 
         pxr_plugin_paths = []
+        ld_path = []
+        python_path =[]
         for resolver in resolver_settings:
-            if resolver["app_name"] != host_variant:
+            if resolver["app_name"] != self.app_name:
                 continue
 
             if resolver["platform"].lower() != system().lower():
                 continue
 
-            self.log.info(f"Initializing asset resolver for {host_variant}.")
+            self.log.info(
+                f"Initializing USD asset resolver for [ {self.app_name} ] .")
             download_dir = get_download_dir()
             with concurrent.futures.ThreadPoolExecutor() as executor:
                 future_file = executor.submit(
@@ -94,14 +95,17 @@ class InitializeAssetResolver(PreLaunchHook):
                 )
                 file = future_file.result()
 
-            resolver_dir = Path(Path(file).stem)
+            resolver_dir = download_dir / Path(Path(file).stem)
             if resolver_dir.is_dir():
                 self.log.info(
-                    f"Existing resolver found in {resolver_dir.as_posix()}.")
+                    "Existing resolver found in "
+                    f"['{resolver_dir.as_posix()}'].")
             else:
                 self.log.info(
-                    f"Extracting resolver to {resolver_dir.as_posix()}.")
-                extract_zip_file(file, download_dir)
+                    f"Extracting resolver to ['{resolver_dir.as_posix()}'].")
+                extract_zip_file(
+                    (download_dir / Path(file)).as_posix(),
+                    download_dir)
 
             pxr_plugin_paths.append(
                 (
@@ -109,8 +113,35 @@ class InitializeAssetResolver(PreLaunchHook):
                     "resources" / "plugInfo.json"
                 ).as_posix()
             )
-            self.log.info(f"Asset resolver {host_variant} initiated.")
+            ld_path.append(
+                (resolver_dir / "ayonUsdResolver" / "lib").as_posix())
+            python_path.append(
+                (resolver_dir / "ayonUsdResolver" /
+                 "lib" / "python").as_posix())
+            self.log.info(f"Asset resolver {self.app_name} initiated.")
+
+        if self.launch_context.env.get("PXR_PLUGINPATH_NAME"):
+            pxr_plugin_paths.append(
+                self.launch_context.env["PXR_PLUGINPATH_NAME"].split(
+                    os.pathsep)
+            )
 
         self.launch_context.env["PXR_PLUGINPATH_NAME"] = os.pathsep.join(
             pxr_plugin_paths
         )
+
+        self.launch_context.env["PYTHONPATH"] += os.pathsep.join(python_path)
+
+        if system().lower() == "windows":
+            self.launch_context.env["PATH"] += os.pathsep.join(ld_path)
+        else:
+            self.launch_context.env["LD_LIBRARY_PATH"] += \
+                os.pathsep.join(ld_path)
+
+        # is there used in the application? Can it hold multiple values?
+        # self.launch_context.env["USD_ASSET_RESOLVER"] = \
+        #   resolver_dir.as_posix()
+
+        # TODO: move debug options to AYON settings
+        self.launch_context.env["TF_DEBUG"] = "1"
+        self.launch_context.env["AYONLOGGERLOGLVL"] = "INFO"
