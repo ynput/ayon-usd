92770c41ac44365ba46101a848a7f52bc1fb1836
Ondřej Samohel <ondrej.samohel@ynput.io>
2024-09-20 19:20:20 +0200

    :bug: fix execution condition and anatomy
    anatomy isn't available on launch context to it needs to be specifically created. Also there was a bug about the condition when this hook should be executed.

--- client/ayon_usd/hooks/usd_pinning_root.py
+++ client/ayon_usd/hooks/usd_pinning_root.py
@@ -1,23 +1,26 @@
 """Pre-launch hook to set USD pinning related environment variable."""
-from typing import ClassVar
-
+from ayon_api import get_folder_by_path, get_project, get_task_by_name
 from ayon_applications import LaunchTypes, PreLaunchHook
+from ayon_core.pipeline import Anatomy
 
 
 class UsdPinningRoot(PreLaunchHook):
     """Pre-launch hook to set USD_ROOT environment variable."""
 
-    app_groups: ClassVar = {"maya", "houdini", "blender", "unreal"}
+    app_groups = {"maya", "houdini", "blender", "unreal"}
     # this should be set to farm_render, but this issue
     # https://github.com/ynput/ayon-applications/issues/2
     # stands in the way
-    launch_types: ClassVar = {LaunchTypes.farm_publish}
+    launch_types = {LaunchTypes.farm_publish}
 
     def execute(self) -> None:
         """Set environments necessary for pinning."""
-        if self.launch_context.env.get("PINNING_FILE_PATH"):
+        if not self.launch_context.env.get("PINNING_FILE_PATH"):
             return
-        anatomy = self.launch_context.anatomy
+
+        project_entity = get_project(self.data.project_name)
+        anatomy = Anatomy(
+            self.data.project_name, project_entity=project_entity)
         self.launch_context.env["PINNING_FILE_PATH"] = anatomy.fill_root(
             self.launch_context.env.get("PINNING_FILE_PATH"),
         )
