a18ffe85f438b488b33791b97683da96b628ea5e
MustafaJafar <mustafataherzaky@outlook.com>
2025-12-02 09:24:58 +0200

    Align pinning export location to render metadata - remove redundant code

--- client/ayon_usd/plugins/publish/extract_skeleton_pinning_json.py
+++ client/ayon_usd/plugins/publish/extract_skeleton_pinning_json.py
@@ -14,6 +14,7 @@ Side effects:
       to check if the processed data is up-to-date.
 
 """
+import os
 import json
 from datetime import datetime
 from pathlib import Path
@@ -22,6 +23,7 @@ from typing import ClassVar
 import pyblish.api
 from ayon_core.pipeline import OptionalPyblishPluginMixin, KnownPublishError
 from ayon_core.pipeline.publish import FARM_JOB_ENV_DATA_KEY
+from ayon_core.pipeline.farm.tools import iter_expected_files
 
 
 class ExtractSkeletonPinningJSON(pyblish.api.InstancePlugin,
@@ -37,6 +39,9 @@ class ExtractSkeletonPinningJSON(pyblish.api.InstancePlugin,
     families: ClassVar = ["usd", "usdrender"]
 
     settings_category: ClassVar = "usd"
+
+    # USD Pinning file generation only supported for Maya and Houdini currently
+    hosts = ["maya", "houdini"]
 
     @staticmethod
     def _has_usd_representation(representations: list) -> bool:
@@ -57,26 +62,21 @@ class ExtractSkeletonPinningJSON(pyblish.api.InstancePlugin,
                 self.log.info("No USD representation found, skipping.")
                 return
 
-        # FIXME: Getting `staging_dir` should be a generic way. `ifdFile` is a very Houdini related.
-        try:
-            staging_dir = Path(instance.data["stagingDir"]) or Path(instance.data["ifdFile"]).parent
-        except KeyError as e:
-            self.log.error("No staging directory found.")
-            raise KnownPublishError("Cannot determine staging directory.") from e
+        pin_file_dir= self.get_pin_file_dir(instance)
+        pin_file = f"{instance.data['productName']}_pin.json"
+        pin_file_path = pin_file_dir.joinpath(pin_file)
 
-        pin_file = f"{staging_dir.stem}_pin.json"
-        pin_file_path = staging_dir.joinpath(pin_file)
         pin_representation = {
             "name": "usd_pinning",
             "ext": "json",
             "files": pin_file_path.name,
-            "stagingDir": staging_dir.as_posix(),
+            "stagingDir": pin_file_dir.as_posix(),
         }
         current_timestamp = datetime.now().timestamp()
         skeleton_pinning_data = {
             "timestamp": current_timestamp,
         }
-        Path.mkdir(staging_dir, parents=True, exist_ok=True)
+        Path.mkdir(pin_file_dir, parents=True, exist_ok=True)
         with open(pin_file_path, "w") as f:
             json.dump(skeleton_pinning_data, f, indent=4)
 
@@ -87,7 +87,52 @@ class ExtractSkeletonPinningJSON(pyblish.api.InstancePlugin,
         if FARM_JOB_ENV_DATA_KEY not in instance.data:
             instance.data[FARM_JOB_ENV_DATA_KEY] = {}
 
+        pin_file_path = self.get_rootless_path(instance, pin_file_path)
         instance.data[FARM_JOB_ENV_DATA_KEY].update({
             "PINNING_FILE_PATH": pin_file_path,
             "ENABLE_STATIC_GLOBAL_CACHE": "1",
         })
+
+    def get_pin_file_dir(self, instance) -> Path:
+        """Get pin file location
+
+        Use the same logic used for obtaining the render metadata json file
+        and default to stagingDir.
+
+        For additional Info, see 
+            ayon_core.pipeline.farm.pyblish_functions.create_metadata_path
+
+        Returns:
+            pin_file_dir(Path): directory for pin file.
+        """
+        pin_file_dir = instance.data.get(
+            "publishRenderMetadataFolder",
+            instance.data.get("outputDir")
+        )
+        if not pin_file_dir:
+            expected_files = instance.data["expectedFiles"]
+            first_file = next(iter_expected_files(expected_files))
+            pin_file_dir = os.path.dirname(first_file)
+
+        if not pin_file_dir:
+            pin_file_dir = instance.data["stagingDir"]
+
+        if pin_file_dir:
+            return Path(pin_file_dir)
+
+        self.log.error("No staging directory found.")
+        raise KnownPublishError("Cannot determine staging directory.")    
+
+    def get_rootless_path(self, instance, path):
+        anatomy = instance.context.data["anatomy"]
+        # Convert output dir to `{root}/rest/of/path/...` with Anatomy
+        success, rootless_path = anatomy.find_root_template_from_path(
+            path)
+        if not success:
+            # `rootless_path` is not set to `output_dir` if none of roots match
+            self.log.warning(
+                f"Could not find root path for remapping '{path}'."
+                " This may cause issues on farm."
+            )
+            rootless_path = path
+        return rootless_path
