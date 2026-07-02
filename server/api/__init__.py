3d280f9d771fc6fea7774a484c65e8ea133edc38
Ondřej Samohel <ondrej.samohel@ynput.io>
2026-07-02 18:40:36 +0200

    fix: fix exports

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,7 +1,7 @@
 """Initialize router for USD Addon."""
 __all__ = [
-    "get_assigned_asset_paths"
-    "router", "router", "get_assigned_asset_paths",
+    "get_assigned_asset_paths",
+    "router",
 ]
 
 from .get_paths_for_assigned_assets import get_assigned_asset_paths
